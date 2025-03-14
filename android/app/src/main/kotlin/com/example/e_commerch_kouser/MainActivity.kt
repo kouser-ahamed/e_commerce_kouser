46b3bb7c9e59c7fba4063a2db401948789805725
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-03-14 23:15:36 +0600

    first main.dart

---
+++ android/app/src/main/kotlin/com/example/e_commerch_kouser/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.e_commerch_kouser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home/page/home.dart';
+
+void main() {
+  runApp(const FreebiesEcom());
+}
+
+class FreebiesEcom extends StatelessWidget {
+  const FreebiesEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Freebies Eco",
+      home: Home(),
+    );
+  }
+}
